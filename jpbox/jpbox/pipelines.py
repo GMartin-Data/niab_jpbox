8c175c43c40a12a16942fd40e2f96c2f7304cb1d
GMartin-Data <gregory.martin.data@gmail.com>
2024-04-07 22:00:19 +0200

    Define JpboxPipeline

--- jpbox/jpbox/pipelines.py
+++ jpbox/jpbox/pipelines.py
@@ -6,8 +6,36 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from loguru import logger
+
+from .utils import convert_dates, convert_duration
 
 
 class JpboxPipeline:
+    @logger.catch
     def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+
+        # jp_duration (convert to minutes)
+        jp_duration = adapter.get("jp_duration")
+        if jp_duration is not None:
+            adapter["jp_duration"] = convert_duration(jp_duration)
+        else:
+            adapter["jp_duration"] = None       
+    
+        # jp_release (convert it to format 'YYYY-MM-DD')
+        jp_release = adapter.get("jp_release")
+        if jp_release is not None:
+            adapter["jp_release"] = convert_dates(jp_release)
+        else:
+            adapter["jp_release"] = None
+
+        # jp_distributors
+        jp_distributors = adapter.get("jp_distributors")
+        try:
+            jp_distributors = [elem.strip() for elem in jp_distributors]
+            adapter["jp_distributors"] = "|".join(jp_distributors)
+        except BaseException:
+            adapter["jp_distributors"] = None 
+
         return item
